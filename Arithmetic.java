80d33bc10633e06cc8b77dcf776fd41eb5aebea7
PushpaSri1306 <puttapushpasri@gmail.com>
2025-01-28 16:23:44 +0530

    Arithmetic Operators

---
+++ Arithmetic.java
@@ -0,0 +1,10 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        Double a = 20.0;
+        Double b = 10.0;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
